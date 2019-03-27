dd71c6f913f41f2dae34f6085b858ffba5665ed9
AD001\z003wftx <tiberiuraicu2@gmail.com>
2019-03-27 17:41:33 +0200

    added animation circuit

--- Software/frontend/src/app/user-page/electric-power-map/electric-power-map.component.ts
+++ Software/frontend/src/app/user-page/electric-power-map/electric-power-map.component.ts
@@ -12,14 +12,42 @@ export class ElectricPowerMapComponent implements OnInit {
   constructor(
     private electricPowerMapServiceService: ElectricPowerMapServiceService
   ) {}
+
   circuits = [];
+
   ngOnInit() {
     this.electricPowerMapServiceService.getCircuits().subscribe(response => {
-      
-        JSON.parse(response._body).forEach(circuit => {
-          this.circuits.push(circuit)
-        });
+      JSON.parse(response._body).forEach(circuit => {
+        circuit["expanded"] = false;
+        circuit["retracted"] = null;
+        circuit["hidden"] = false;
+        this.circuits.push(circuit);
+
+        console.log(circuit);
+      });
     });
   }
+  changeSize(circuit) {
+    this.circuits.map(mappedCircuit => {
+      if (mappedCircuit.circuitId != circuit.circuitId)
+        mappedCircuit.retracted = null;
+    });
+    if (circuit.expanded == false) {
+      circuit.retracted = false;
+      circuit.expanded = true;
+    } else if (circuit.expanded == true) {
+      circuit.retracted = true;
+      circuit.expanded = false;
+     
+    }
+
+    this.circuits.map(mappedCircuit => {
+      if (mappedCircuit.circuitId != circuit.circuitId)
+        mappedCircuit.hidden = !mappedCircuit.hidden;
+    });
+    this.circuits.map(mappedCircuit => {
+      console.log(mappedCircuit);
+    });
   
+  }
 }
